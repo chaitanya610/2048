7051adeaeeea9e96a1d379d5869070f968f166f0
chaitanya <saichaitanya080@gmail.com>
2017-12-16 11:13:24 +0530

    Update MainActivity.java

--- main/java/com/example/android/a2048/MainActivity.java
+++ main/java/com/example/android/a2048/MainActivity.java
@@ -79,53 +79,6 @@ public class MainActivity extends AppCompatActivity implements OnGestureListener
             return true ;
         }
     }
-
-    @Override
-    public void onLongPress(MotionEvent arg0) {
-
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public boolean onScroll(MotionEvent arg0, MotionEvent arg1, float arg2, float arg3) {
-
-        // TODO Auto-generated method stub
-
-        return false;
-    }
-
-    @Override
-    public void onShowPress(MotionEvent arg0) {
-
-        // TODO Auto-generated method stub
-
-    }
-
-    @Override
-    public boolean onSingleTapUp(MotionEvent arg0) {
-
-        // TODO Auto-generated method stub
-
-        return false;
-    }
-
-    @Override
-    public boolean onTouchEvent(MotionEvent motionEvent) {
-
-        // TODO Auto-generated method stub
-
-        return gestureDetector.onTouchEvent(motionEvent);
-    }
-
-    @Override
-    public boolean onDown(MotionEvent arg0) {
-
-        // TODO Auto-generated method stub
-
-        return false;
-    }
-
     public static void copy(int a[][],int b[][])
     {
         int i,j;
